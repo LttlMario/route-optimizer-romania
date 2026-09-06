3791e8b6dfd4d88a3b0989ff1ec13dc833e84e8c
Route Optimizer <route-optimizer@localhost>
2026-09-06 03:25:59 +0300

    Group delivery history by day

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'route-optimizer-pwa-v46';
+const CACHE_NAME = 'route-optimizer-pwa-v47';
 const APP_SHELL = [
   './', './index.html', './routes.html', './completed.html',
   './styles.css', './routes.css', './app.js', './routes.js', './completed.js',
@@ -20,6 +20,7 @@ self.addEventListener('fetch', (event) => {
     return response;
   }).catch(() => caches.match('./index.html'))));
 });
+
 
 
 
